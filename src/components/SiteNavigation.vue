eb5a824b395f5ec1b8810972ad7cda33a2cfd896
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-09-15 10:56:21 -0500

    encabezado y buscador creado

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -2,13 +2,16 @@
   <header class="sticky top-0 bg-primary shadow-lg">
     <nav class="container flex flex-col sm:flex-row
     items-center gap-4 text-blue py-6">
+    <RouterLink :to="{name:'home'}">
       <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-sum text-2x1"></i>
+        <i class="fa-solid fa-sum text-2x1">SearchApp</i>
       </div>
+    </RouterLink>
     </nav>
   </header>
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router';
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup>
-</script>
 
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" placeholder="Search" class="w-full h-12 px-4 rounded-full bg-secondary text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" />
+    </div>
   </main>
 </template>
+
+  <script setup>
+  </script>
