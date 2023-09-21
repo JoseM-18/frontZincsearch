c33e46a788b3477fd4805dd2938795d37f7dab98
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-09-21 15:56:21 -0500

    mejora visual del sistema

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,43 +10,48 @@
         Search
       </button>
     </div>
-    <ul v-if="searchResults">
       <div class=" flex  sm:flex-row ">
         <div class="col-lg-8 offset-lg-2  ">
+          <div v-if="cargando" class="flex center item-center space-x-4 ">
+            <div class="spinner-border text-secondary flex justify-center" role="status">
+              <span class="visually-hidden ">Loading...</span>
+            </div>
+          </div>
           <div v-if="emails" class=" table-responsive flex center item-center space-x-4 ">
             <table id="tabla" class="table table-striped table-bordered table-hover" style="width:100%">
               <thead>
                 <tr>
                   <th>#</th>
+                  <th>Date</th>
                   <th>From</th>
                   <th>To</th>
                   <th>Subject</th>
+                  <th class="display display-none">Body</th> 
                 </tr>
               </thead>
               <tbody>
                 <tr v-for="(email, index) in emails" :key="index">
                   <td>{{ index + 1 }}</td>
+                  <td>{{ email._source.date }}</td>
                   <td>{{ email._source.from }}</td>
-                  <td>
-                    <span v-if="showFullContent">{{ email._source.to }}</span>
-                    <span v-else>{{ email._source.to.slice(0, 50) }}...</span>
-                  </td>
+                  <td>{{ seeAllContent(email._source.to, index) }}</td>
                   <td>{{ email._source.subject }}</td>
+                  <td class="display display-none">{{ email._source.body }}</td>
+                  
                   <button
-                    class="btn btn-primary mb-4 transition delay-150 duration-300 ease-in-out bg-cyan-700 rounded-full h-7 w-7 hover:bg-sky-500 mx-2"
-                    @click="expandBody(email._source.body)">
-                    {{ showFullContent ? '-' : '+' }}
+                    class="btn btn-primary mb-4 transition delay-150 duration-300 ease-in-out bg-cyan-700 rounded-full h-7 w-7 hover:bg-sky-500 mx-2 overflow-clip mt-5"
+                    @click="expandBody(email._source.body, index)">
+                    {{ showFullContent && indexEmail === index ? "-" : "+" }}
                   </button>
                 </tr>
               </tbody>
             </table>
-            <textarea v-if="mostrar" disabled class="form-control bg-tertiary border-none" rows="20" cols="65"
+            <textarea v-if="showFullContent" disabled rows="20" cols="60" class="mt-20 px-4 rounded-lg py-2 border border-gray-300 form-control bg-tertiary border-none"
               id="comment" v-model="body">
             </textarea>
           </div>
         </div>
       </div>
-    </ul>
 
   </main>
 </template>
@@ -66,9 +71,10 @@ export default {
       searchQuery: "",
       searchResults: [],
       emails: null,
-      mostrar: false,
       body: null,
       showFullContent: false,
+      indexEmail: null,
+      cargando: false,
     };
   },
   mounted() {
@@ -87,29 +93,57 @@ export default {
           language: {
             url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json",
           },
+          columnDefs: [
+            {
+              targets: [5],
+              visible: false,
+              searchable: true,
+            },
+          ],
         });
       });
     },
     async getSearchQuery() {
-      if (this.searchQuery !== "") {
-        const result = await axios.get(`http://localhost:9090/search?q=${this.searchQuery}`);
-        this.searchResults = result.data;
-        this.emails = this.searchResults.hits.hits;
-        if ($.fn.DataTable.isDataTable("#tabla")) {
-          $("#tabla").DataTable().destroy();
+      try {
+        if (this.searchQuery !== "") {
+          this.cargando = true;
+          this.body = null;
+          this.showFullContent = false;
+          this.indexEmail = null;
+          const result = await axios.get(`http://localhost:9090/search?q=${this.searchQuery}`);
+          this.searchResults = result.data;
+          this.emails = this.searchResults.hits.hits;
+          this.cargando = false;
+          if ($.fn.DataTable.isDataTable("#tabla")) {
+            $("#tabla").DataTable().destroy();
+          }
+          this.getTabla();
+        } else {
+          this.searchResults = [];
         }
-        this.getTabla();
-      } else {
-        this.searchResults = [];
+      } catch (error) {
+        console.log(error);
       }
     },
-    expandBody(body) {
-      this.mostrar = !this.mostrar;
-      this.body = body;
-      this.showFullContent = !this.showFullContent;
+    expandBody(body, index) {
+      
+        this.body = body;
+        this.showFullContent = !this.showFullContent;
+        this.indexEmail = index;
+        if (!this.showFullContent) {
+          this.body = null;
+          this.indexEmail = null;
+        }
     },
+    seeAllContent(data, index) {
+      if (data.length > 50 && !this.showFullContent || 
+      data.length > 50 && this.showFullContent && this.indexEmail !== index) {
+        return data.slice(0, 50) + "...";
+      } else {
+        return data;
+      }
 
-
+    },
   },
 };
 </script>
